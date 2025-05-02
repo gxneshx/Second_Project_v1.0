2da2e04967aa12a08d6ad30bd8529a080debf808
gxneshx <kalchenko.michael@gmail.com>
2025-05-03 01:48:32 +0300

    First commit with already created frontend side without logic yet

---
+++ src/js/images.js
@@ -0,0 +1,4 @@
+const uploadButton = document.querySelector('.upload-pictures-button')
+uploadButton.addEventListener('click', () => {
+    window.open("upload.html", "_self");
+})
---
+++ src/js/upload.js
@@ -0,0 +1,4 @@
+const imagesButton = document.querySelector('.uploaded-images-button')
+imagesButton.addEventListener('click', () => {
+    window.open("images.html", "_self");
+})
